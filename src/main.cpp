ee2c1ebb942f1fc15a845ba0f46dc1fccd780063
Otto Link <otto.link.bv@gmail.com>
2023-08-01 16:52:14 +0200

    Update submodule Highmap

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
-#include "highmap/array.hpp"
-#include "highmap/primitives.hpp"
+#include "highmap.hpp"
 
 int main()
 {
-  hmap::Array array = hmap::constant({8, 8}, 1.f);
+  hmap::Array array = hmap::constant(hmap::Vec2(8, 8), 1.f);
   array.infos();
-  
+
   std::cout << "ok" << std::endl;
   return 0;
 }
