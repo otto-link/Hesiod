6d81a44e1acb4bae51438bc16327a2405e9d33f4
Otto Link <otto.link.bv@gmail.com>
2023-08-27 13:37:59 +0200

    Add gaussian pulse primitive node

---
+++ src/nodes/control_node/gaussian_pulse.cpp
@@ -0,0 +1,60 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+#include "macrologger.h"
+
+#include "hesiod/control_node.hpp"
+
+namespace hesiod::cnode
+{
+
+GaussianPulse::GaussianPulse(std::string     id,
+                             hmap::Vec2<int> shape,
+                             hmap::Vec2<int> tiling,
+                             float           overlap)
+    : Primitive(id, shape, tiling, overlap)
+{
+  LOG_DEBUG("GaussianPulse::GaussianPulse()");
+  this->node_type = "GaussianPulse";
+  this->category = category_mapping.at(this->node_type);
+  this->remove_port("dy");
+  this->value_out.set_sto(shape, tiling, overlap);
+  this->update_inner_bindings();
+}
+
+void GaussianPulse::compute()
+{
+  LOG_DEBUG("computing GaussianPulse node [%s]", this->id.c_str());
+
+  std::function<hmap::Array(hmap::Vec2<int>,
+                            hmap::Vec2<float>,
+                            hmap::Vec2<float>,
+                            hmap::Array *)>
+      lambda;
+
+  if (this->inverse)
+    lambda = [this](hmap::Vec2<int>   shape,
+                    hmap::Vec2<float> shift,
+                    hmap::Vec2<float> scale,
+                    hmap::Array      *p_noise_x)
+    {
+      return 1.f -
+             hmap::gaussian_pulse(shape, this->sigma, p_noise_x, shift, scale);
+    };
+  else
+    lambda = [this](hmap::Vec2<int>   shape,
+                    hmap::Vec2<float> shift,
+                    hmap::Vec2<float> scale,
+                    hmap::Array      *p_noise_x) {
+      return hmap::gaussian_pulse(shape, this->sigma, p_noise_x, shift, scale);
+    };
+
+  hmap::fill(this->value_out,
+             (hmap::HeightMap *)this->get_p_data("dx"),
+             lambda);
+
+  // remap the output
+  this->value_out.remap(this->vmin, this->vmax);
+}
+
+} // namespace hesiod::cnode
---
+++ src/nodes/view_node/view_gaussian_pulse.cpp
@@ -0,0 +1,37 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+#include "macrologger.h"
+
+#include "hesiod/view_node.hpp"
+
+namespace hesiod::vnode
+{
+
+ViewGaussianPulse::ViewGaussianPulse(std::string     id,
+                                     hmap::Vec2<int> shape,
+                                     hmap::Vec2<int> tiling,
+                                     float           overlap)
+    : ViewNode(), hesiod::cnode::GaussianPulse(id, shape, tiling, overlap)
+{
+  this->set_p_control_node((gnode::Node *)this);
+  this->set_preview_port_id("output");
+  this->set_view2d_port_id("output");
+}
+
+bool ViewGaussianPulse::render_settings()
+{
+  bool has_changed = false;
+  has_changed |= this->render_settings_header();
+
+  ImGui::Checkbox("inverse", &this->inverse);
+  has_changed |= this->trigger_update_after_edit();
+
+  ImGui::SliderFloat("sigma", &this->sigma, 1.f, 512.f, "%.1f");
+  has_changed |= this->trigger_update_after_edit();
+
+  has_changed |= this->render_settings_footer();
+  return has_changed;
+}
+
+} // namespace hesiod::vnode
