2b2a6a5ff05fbce9e6b8d66a2fe33d5679da2642
Otto Link <otto.link.bv@gmail.com>
2023-09-01 13:40:01 +0200

    Fix could interp for empty cloud

--- src/nodes/control_node/cloud_to_array_interp.cpp
+++ src/nodes/control_node/cloud_to_array_interp.cpp
@@ -31,25 +31,29 @@ void CloudToArrayInterp::compute()
   hmap::Cloud *p_input_cloud = static_cast<hmap::Cloud *>(
       (void *)this->get_p_data("cloud"));
 
-  hmap::fill(this->value_out,
-             (hmap::HeightMap *)this->get_p_data("dx"),
-             (hmap::HeightMap *)this->get_p_data("dy"),
-             [this, p_input_cloud](hmap::Vec2<int>   shape,
-                                   hmap::Vec2<float> shift,
-                                   hmap::Vec2<float> scale,
-                                   hmap::Array      *p_noise_x,
-                                   hmap::Array      *p_noise_y)
-             {
-               hmap::Array array(shape);
-               p_input_cloud->to_array_interp(array,
-                                              {0.f, 1.f, 0.f, 1.f},
-                                              this->interpolation_method,
-                                              p_noise_x,
-                                              p_noise_y,
-                                              shift,
-                                              scale);
-               return array;
-             });
+  if (p_input_cloud->get_npoints() > 0)
+    hmap::fill(this->value_out,
+               (hmap::HeightMap *)this->get_p_data("dx"),
+               (hmap::HeightMap *)this->get_p_data("dy"),
+               [this, p_input_cloud](hmap::Vec2<int>   shape,
+                                     hmap::Vec2<float> shift,
+                                     hmap::Vec2<float> scale,
+                                     hmap::Array      *p_noise_x,
+                                     hmap::Array      *p_noise_y)
+               {
+                 hmap::Array array(shape);
+                 p_input_cloud->to_array_interp(array,
+                                                {0.f, 1.f, 0.f, 1.f},
+                                                this->interpolation_method,
+                                                p_noise_x,
+                                                p_noise_y,
+                                                shift,
+                                                scale);
+                 return array;
+               });
+  else
+    // fill with zeroes
+    hmap::transform(this->value_out, [](hmap::Array array) { array = 0.f; });
 
   // remap the output
   this->value_out.remap(this->vmin, this->vmax);
