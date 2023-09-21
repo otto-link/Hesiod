a1e79feed58b61d07e5590aabbade294ccf27cf0
Otto Link <otto.link.bv@gmail.com>
2023-09-21 23:42:18 +0200

    Add default port for 2D viewer

--- src/nodes/view_node/view_value_noise_delaunay.cpp
+++ src/nodes/view_node/view_value_noise_delaunay.cpp
@@ -20,6 +20,7 @@ ViewValueNoiseDelaunay::ViewValueNoiseDelaunay(std::string     id,
   LOG_DEBUG("ViewValueNoiseDelaunay::ViewValueNoiseDelaunay()");
   this->set_p_control_node((gnode::Node *)this);
   this->set_preview_port_id("output");
+  this->set_view2d_port_id("output");
 }
 
 bool ViewValueNoiseDelaunay::render_settings()
