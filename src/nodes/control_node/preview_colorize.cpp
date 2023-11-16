947e285dab801d3c35289483f9be3aac3849c54e
Otto Link <otto.link.bv@gmail.com>
2023-11-16 20:42:31 +0100

    Fix thru port issue with failed downstream update triggering (#66)

--- src/nodes/control_node/preview_colorize.cpp
+++ src/nodes/control_node/preview_colorize.cpp
@@ -28,7 +28,9 @@ void PreviewColorize::update_inner_bindings()
 
 void PreviewColorize::compute()
 {
-  // nothing here
+  // for thru port
+  this->update_inner_bindings();
+  this->update_links();
 }
 
 } // namespace hesiod::cnode
