97f575b24b44eee1f96642cf23e785fe4a09ca06
Otto Link <otto.link.bv@gmail.com>
2023-12-13 10:17:22 +0100

    Add automatic date and hash to AppImage binary file name

--- scripts/generate_appimage/generate_appimage.sh
+++ scripts/generate_appimage/generate_appimage.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -rf AppDir
 
 wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -P ${2}
@@ -5,4 +7,9 @@ chmod 0744 ${2}/linuxdeploy-x86_64.AppImage
 
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --executable ${3}/bin/hesiod --output appimage -d ${1}/hesiod.desktop -i ${1}/icon_hesiod.png
 
-cp hesiod-x86_64.AppImage ${3}/bin/.
+HASH=`git rev-parse HEAD`
+HASH=${HASH:0:7}
+
+DATE=$(date '+%Y-%m-%d')
+
+cp hesiod-x86_64.AppImage ${3}/bin/hesiod-x86_64_${DATE}_${HASH}.AppImage
