24b9927f82100e3b304269e3d384503d820b840f
Hendrik A <lightjavacode@gmail.com>
2024-02-24 03:21:12 +0100

    Beginning to implement JSON-Serialization.
    - Added basic interfaces.
    - Added batch primitive serializer.
    - Added stubs.

---
+++ Hesiod/include/hesiod/serialization.hpp
@@ -0,0 +1,229 @@
+#pragma once
+#include <cstring>
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace hesiod::serialization
+{
+
+    class SerializationBase
+    {
+    public:
+        virtual ~SerializationBase() = default;
+        virtual bool serialize_json_v2(std::string, nlohmann::json&) { return false; }
+        virtual bool deserialize_json_v2(std::string, nlohmann::json&) { return false; }
+    };
+
+    enum class SerializationBatchHelperElementType : char
+    {
+        BYTE = 0,
+        CHARACTER = 1,
+        UNSIGNED_SHORT = 2,
+        SHORT = 3,
+        UNSIGNED_INT = 4,
+        INT = 5,
+        LONG_LONG = 6,
+        UNSIGNED_LONG_LONG = 7,
+        FLOAT = 8,
+        DOUBLE = 9,
+        STL_STRING = 10,
+        OBJECT = 11,
+        OBJECT_PTR = 12,
+        INVALID = 13
+    };
+
+    class SerializationBatchHelperElement
+    {
+    public:
+        SerializationBatchHelperElement():
+            name(),
+            data(),
+            type(SerializationBatchHelperElementType::INVALID)
+        {}
+
+        SerializationBatchHelperElement(SerializationBatchHelperElement& S)
+        {
+            this->operator=(S);
+        }
+
+        ~SerializationBatchHelperElement() = default;
+
+        SerializationBatchHelperElement& operator=(SerializationBatchHelperElement s)
+        {
+            this->name = s.name;
+            this->data = s.data;
+            this->type = s.type;
+
+            return *this;
+        }
+    public:
+        std::string name;
+        void* data;
+        SerializationBatchHelperElementType type;
+    };
+
+    class SerializationBatchHelper : SerializationBase
+    {
+    public:
+        SerializationBatchHelper();
+        ~SerializationBatchHelper();
+
+        bool serialize_json_v2(std::string fieldName, nlohmann::json& outputData);
+        bool deserialize_json_v2(std::string fieldName, nlohmann::json& inputData);
+
+        // Wrappers for the ease of use
+
+        SerializationBatchHelper& AddByte(std::string key, unsigned char* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::BYTE;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddCharacter(std::string key, char* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::CHARACTER;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddUnsignedShort(std::string key, unsigned short* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::UNSIGNED_SHORT;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddShort(std::string key, short* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::SHORT;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddUnsignedInt(std::string key, unsigned int* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::UNSIGNED_INT;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddInt(std::string key, int* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::INT;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddLongLong(std::string key, long long* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::LONG_LONG;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddUnsignedLongLong(std::string key, unsigned long long* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::UNSIGNED_LONG_LONG;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddFloat(std::string key, float* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::FLOAT;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddDouble(std::string key, double* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::DOUBLE;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddStlString(std::string key, std::string* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::STL_STRING;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddObject(std::string key, SerializationBase* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::OBJECT;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+
+        SerializationBatchHelper& AddObjectPtr(std::string key, SerializationBase* value) 
+        {
+            SerializationBatchHelperElement* e = new SerializationBatchHelperElement();
+            e->name = key;
+            e->data = value;
+            e->type = SerializationBatchHelperElementType::OBJECT_PTR;
+            this->elements.push_back(e);
+
+            return *this;
+        }
+    private:
+        std::vector<SerializationBatchHelperElement*> elements;
+    };
+
+    class SerializationBatchBase : public SerializationBase
+    {
+    public:
+    private:
+        
+    };
+
+}
---
+++ Hesiod/src/serialization.cpp
@@ -0,0 +1,143 @@
+#include "hesiod/serialization.hpp"
+#include "macro-logger/macrologger.h"
+#include <string>
+
+namespace hesiod::serialization 
+{
+    #define SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(Enum, Cast)    case SerializationBatchHelperElementType::Enum: \
+                                                                        { \
+                                                                            batchData[e->name] = *reinterpret_cast< Cast *>(e->data); \
+                                                                            break; \
+                                                                        }
+
+    #define SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(Enum, Cast) case SerializationBatchHelperElementType::Enum: \
+                                                                        { \
+                                                                            *reinterpret_cast< Cast *>(e->data) = batchData[e->name].get< Cast >(); \
+                                                                            break; \
+                                                                        }
+
+    SerializationBatchHelper::SerializationBatchHelper()
+    {}
+
+    SerializationBatchHelper::~SerializationBatchHelper()
+    {}
+    
+    bool SerializationBatchHelper::serialize_json_v2(std::string fieldName, nlohmann::json& outputData)
+    {
+        nlohmann::json batchData = nlohmann::json();
+
+        for(SerializationBatchHelperElement* e : this->elements)
+        {
+            if(e->data == nullptr) continue;
+
+            switch (e->type) {
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(BYTE, unsigned char);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(CHARACTER, char);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(UNSIGNED_SHORT, unsigned short);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(SHORT, short);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(UNSIGNED_INT, unsigned int);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(INT, int);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(LONG_LONG, long long);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(UNSIGNED_LONG_LONG, unsigned long long);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(FLOAT, float);
+                SERIALIZATION_BATCH_IMPLEMENT_SERIALIZE_CASE(DOUBLE, double);
+                case SerializationBatchHelperElementType::STL_STRING:
+                {
+                    batchData[e->name] = reinterpret_cast<std::string*>(e->data)->data();
+                    break;
+                }
+                case SerializationBatchHelperElementType::OBJECT:
+                {
+                    nlohmann::json tempData = nlohmann::json();
+
+                    SerializationBase* base = reinterpret_cast<SerializationBase*>(e->data);
+
+                    if(base) base->serialize_json_v2("sub", tempData);
+
+                    batchData[e->name] = tempData;
+                    break;
+                }
+                case SerializationBatchHelperElementType::OBJECT_PTR:
+                {
+                    nlohmann::json tempData = nlohmann::json();
+
+                    SerializationBase* base = (*reinterpret_cast<SerializationBase**>(e->data));
+
+                    if(base) base->serialize_json_v2("sub", tempData);
+
+                    batchData[e->name] = tempData;
+                    break;
+                }
+                default:
+                    LOG_ERROR("[%s] Unimplemented type.", __FUNCTION__);
+                    break;
+            }
+        }
+
+        outputData[fieldName] = batchData;
+
+        return true;
+    }
+
+    bool SerializationBatchHelper::deserialize_json_v2(std::string fieldName, nlohmann::json& inputData)
+    {
+        if(inputData[fieldName].is_null() || inputData[fieldName].is_object() == false)
+        {
+            LOG_ERROR("[%s] Encountered error while parsing object.", __FUNCTION__);
+            return false;
+        }
+
+        nlohmann::json batchData = inputData[fieldName];
+
+        for(SerializationBatchHelperElement* e : this->elements)
+        {
+            if(e->data == nullptr) continue;
+            if(batchData[e->name].is_null()) continue;
+
+            switch (e->type) 
+            {
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(BYTE, unsigned char);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(CHARACTER, char);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(UNSIGNED_SHORT, unsigned short);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(SHORT, short);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(UNSIGNED_INT, unsigned int);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(INT, int);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(LONG_LONG, long long);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(UNSIGNED_LONG_LONG, unsigned long long);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(FLOAT, float);
+                SERIALIZATION_BATCH_IMPLEMENT_DESERIALIZE_CASE(DOUBLE, double);
+                case SerializationBatchHelperElementType::STL_STRING:
+                {
+                    std::string deserializedString = batchData[e->name].get<std::string>();
+                    reinterpret_cast<std::string*>(e->data)->swap(deserializedString);
+                    break;
+                }
+                case SerializationBatchHelperElementType::OBJECT:
+                {
+                    nlohmann::json tempData = batchData[e->name];
+                    SerializationBase* base = reinterpret_cast<SerializationBase*>(e->data);
+
+                    if(base) base->deserialize_json_v2("sub", tempData);
+                    
+                    break;
+                }
+                case SerializationBatchHelperElementType::OBJECT_PTR:
+                {
+                    nlohmann::json tempData = batchData[e->name];
+                    SerializationBase* base = (*reinterpret_cast<SerializationBase**>(e->data));
+
+                    if(base) base->deserialize_json_v2("sub", tempData);
+
+                    break;
+                }
+                default:
+                    LOG_ERROR("[%s] Unimplemented type.", __FUNCTION__);
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+
+}
