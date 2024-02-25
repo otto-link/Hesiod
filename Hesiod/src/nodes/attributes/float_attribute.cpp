ecefe85e3f14c91e6795bca653846837621d6854
Hendrik A <lightjavacode@gmail.com>
2024-02-25 22:23:06 +0100

    Implemented FloatAttribute

--- Hesiod/src/nodes/attributes/float_attribute.cpp
+++ Hesiod/src/nodes/attributes/float_attribute.cpp
@@ -38,4 +38,31 @@ bool FloatAttribute::render_settings(std::string label)
   return has_changed;
 }
 
+bool FloatAttribute::serialize_json_v2(std::string fieldName, nlohmann::json& outputData) 
+{ 
+  outputData[fieldName]["value"] = this->value;
+  outputData[fieldName]["vmin"] = this->vmin;
+  outputData[fieldName]["vmax"] = this->vmax;
+  return true; 
+}
+
+bool FloatAttribute::deserialize_json_v2(std::string fieldName, nlohmann::json& inputData) 
+{ 
+  if(
+    inputData[fieldName].is_null() == true ||
+    inputData[fieldName]["value"].is_number() == false ||
+    inputData[fieldName]["vmin"].is_number() == false ||
+    inputData[fieldName]["vmax"].is_number() == false
+  )
+  {
+    LOG_DEBUG("[%s] Attribute %s is not valid.", __FUNCTION__, fieldName.data());
+    return false;
+  }
+
+  this->value = inputData[fieldName]["value"].get<float>();
+  this->vmin = inputData[fieldName]["vmin"].get<float>();
+  this->vmax = inputData[fieldName]["vmax"].get<float>();
+  return true; 
+}
+
 } // namespace hesiod
