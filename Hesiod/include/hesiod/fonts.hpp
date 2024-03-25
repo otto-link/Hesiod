9016782da5d8929bf6024a923826d37f7c347194
Otto Link <otto.link.bv@gmail.com>
2024-03-25 21:14:42 +0100

    Strip down to minimal sources...

--- Hesiod/include/hesiod/fonts.hpp
+++
@@ -1,7 +0,0 @@
-/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
- * Public License. The full license is in the file LICENSE, distributed with
- * this software. */
-#pragma once
-#include <vector>
-
-std::vector<unsigned char> &get_robot_reguar_ttf();
--- external/ImCandy.cmake
+++
@@ -1,8 +0,0 @@
-project(ImCandy)
-
-set(IMCANDY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/ImguiCandy)
-
-add_library(${PROJECT_NAME} INTERFACE)
-add_library(${PROJECT_NAME}::${PROJECT_NAME} ALIAS ${PROJECT_NAME})
-
-target_include_directories(${PROJECT_NAME} INTERFACE ${IMCANDY_DIR})
