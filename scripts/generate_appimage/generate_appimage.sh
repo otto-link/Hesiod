277d1345d69976d9e465bd7fc9fafec427fbec3e
Otto Link <otto.link.bv@gmail.com>
2024-04-06 09:24:07 +0200

    Fix cmake-multi-platform.yml

--- scripts/generate_appimage/generate_appimage.sh
+++ scripts/generate_appimage/generate_appimage.sh
@@ -3,7 +3,10 @@
 rm -rf AppDir
 
 wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -P ${2}
+wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage -P ${2}
+
 chmod 0744 ${2}/linuxdeploy-x86_64.AppImage
+chmod 0744 ${2}/linuxdeploy-plugin-qt-x86_64.AppImage
 
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --executable ${3}/build/bin/hesiod --plugin qt --output appimage -d ${1}/hesiod.desktop -i ${1}/icon_hesiod.png
 
