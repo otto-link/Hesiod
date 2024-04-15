b4d285d2327d36a01c0c83d78d1838942d40f0f7
Otto Link <otto.link.bv@gmail.com>
2024-04-15 19:55:33 +0200

    Add Plateau node

--- Hesiod/include/hesiod/model/nodes.hpp
+++ Hesiod/include/hesiod/model/nodes.hpp
@@ -345,6 +345,32 @@ protected:
 };
 
 /**
+ * @brief Plateau class.
+ */
+class Plateau : public BaseNode
+{
+public:
+  Plateau(const ModelConfig *p_config);
+
+  QtNodes::NodeData *get_preview_data() { return this->out.get(); }
+  QtNodes::NodeData *get_viewer2d_data() { return this->out.get(); }
+  QtNodes::NodeData *get_viewer3d_data() { return this->out.get(); }
+  QtNodes::NodeData *get_viewer3d_color() { return this->mask.lock().get(); }
+
+  void compute() override;
+
+  std::shared_ptr<QtNodes::NodeData> outData(QtNodes::PortIndex port_index) override;
+
+  void setInData(std::shared_ptr<QtNodes::NodeData> data,
+                 QtNodes::PortIndex                 port_index) override;
+
+protected:
+  std::weak_ptr<HeightMapData>   in;
+  std::weak_ptr<MaskData>        mask;
+  std::shared_ptr<HeightMapData> out;
+};
+
+/**
  * @brief Remap class.
  */
 class Remap : public BaseNode
---
+++ Hesiod/src/model/nodes/plateau.cpp
@@ -0,0 +1,90 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+#include "hesiod/model/nodes.hpp"
+
+namespace hesiod
+{
+
+Plateau::Plateau(const ModelConfig *p_config) : BaseNode(p_config)
+{
+  LOG_DEBUG("Plateau::Plateau");
+
+  // model
+  this->node_caption = "Plateau";
+
+  // inputs
+  this->input_captions = {"input", "mask"};
+  this->input_types = {HeightMapData().type(), MaskData().type()};
+
+  // outputs
+  this->output_captions = {"output"};
+  this->output_types = {HeightMapData().type()};
+  this->out = std::make_shared<HeightMapData>(p_config);
+
+  // attributes
+  this->attr["radius"] = NEW_ATTR_FLOAT(0.05f, 0.01f, 0.5f);
+  this->attr["factor"] = NEW_ATTR_FLOAT(4.f, 0.01f, 10.f);
+  this->attr_ordered_key = {"radius", "factor"};
+
+  // update
+  this->compute();
+}
+
+std::shared_ptr<QtNodes::NodeData> Plateau::outData(QtNodes::PortIndex /* port_index */)
+{
+  return std::static_pointer_cast<QtNodes::NodeData>(this->out);
+}
+
+void Plateau::setInData(std::shared_ptr<QtNodes::NodeData> data,
+                        QtNodes::PortIndex                 port_index)
+{
+  if (!data)
+    Q_EMIT this->dataInvalidated(0);
+
+  switch (port_index)
+  {
+  case 0:
+    this->in = std::dynamic_pointer_cast<HeightMapData>(data);
+    break;
+  case 1:
+    this->mask = std::dynamic_pointer_cast<MaskData>(data);
+  }
+
+  this->compute();
+}
+
+// --- computing
+
+void Plateau::compute()
+{
+  Q_EMIT this->computingStarted();
+
+  LOG_DEBUG("computing node [%s]", this->name().toStdString().c_str());
+
+  hmap::HeightMap *p_in = HSD_GET_POINTER(this->in);
+  hmap::HeightMap *p_mask = HSD_GET_POINTER(this->mask);
+  hmap::HeightMap *p_out = this->out->get_ref();
+
+  if (p_in)
+  {
+    // copy the input heightmap
+    *p_out = *p_in;
+
+    int ir = std::max(1, (int)(GET_ATTR_FLOAT("radius") * p_out->shape.x));
+
+    hmap::transform(*p_out,
+                    p_mask,
+                    [this, &ir](hmap::Array &x, hmap::Array *p_mask)
+                    { hmap::plateau(x, p_mask, ir, GET_ATTR_FLOAT("factor")); });
+    p_out->smooth_overlap_buffers();
+  }
+
+  // propagate
+  QtNodes::PortIndex const out_port_index = 0;
+
+  Q_EMIT this->computingFinished();
+  Q_EMIT this->dataUpdated(out_port_index);
+}
+
+} // namespace hesiod
