20018083e3555e977e24951c0347a74cf2e62998
Otto Link <otto.link.bv@gmail.com>
2024-04-21 12:59:17 +0200

    Move Comment node GUI sources to a specific source file bis

--- Hesiod/src/model/nodes/comment.cpp
+++ Hesiod/src/model/nodes/comment.cpp
@@ -6,6 +6,8 @@
 namespace hesiod
 {
 
+// see comment_gui.cpp for GUI-specific sources
+
 Comment::Comment(const ModelConfig *p_config) : BaseNode(p_config)
 {
   LOG_DEBUG("Comment::Comment");
@@ -37,39 +39,6 @@ void Comment::compute()
   // empty
 
   Q_EMIT this->computingFinished();
-}
-
-// --- GUI
-
-QWidget *Comment::embeddedWidget()
-{
-  if (!this->label_comment)
-  {
-    this->label_comment = new QLabel();
-    this->label_comment->setMinimumSize(128, 128);
-    this->label_comment->setMaximumSize(128, 128);
-    this->label_comment->setStyleSheet("background-color: #3B3B3B; color: #DFE1E2");
-    this->label_comment->setAlignment(Qt::AlignTop);
-    this->label_comment->setWordWrap(true);
-
-    QFont font = this->label_comment->font();
-    font.setPointSize(font.pointSize() - 3);
-    font.setItalic(true);
-    this->label_comment->setFont(font);
-
-    this->label_comment->setText(GET_ATTR_STRING("comment").c_str());
-
-    connect(this,
-            &BaseNode::node_loaded,
-            [this]()
-            { this->label_comment->setText(GET_ATTR_STRING("comment").c_str()); });
-
-    connect(this,
-            &BaseNode::settings_changed,
-            [this]()
-            { this->label_comment->setText(GET_ATTR_STRING("comment").c_str()); });
-  }
-  return (QWidget *)this->label_comment;
 }
 
 } // namespace hesiod
