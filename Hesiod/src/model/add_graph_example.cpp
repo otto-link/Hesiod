2e47bf8caf634b6b1d1f8fcc69b7d66c491c962d
Otto Link <otto.link.bv@gmail.com>
2024-04-26 17:30:06 +0200

    Add MakeBinary node

--- Hesiod/src/model/add_graph_example.cpp
+++ Hesiod/src/model/add_graph_example.cpp
@@ -74,6 +74,25 @@ QtNodes::NodeId add_graph_example(HsdDataFlowGraphModel *p_model,
     return node_id2;
   }
   //
+  else if (node_type == "MakeBinary")
+  {
+    QtNodes::NodeId node_id1 = p_model->addNode("NoiseFbm");
+    QtNodes::NodeId node_id2 = p_model->addNode(QString::fromStdString(node_type));
+
+    p_model->addConnection(QtNodes::ConnectionId(node_id1, 0, node_id2, 0));
+
+    p_model->setNodeData(node_id1, QtNodes::NodeRole::Position, QPointF(0.f, 0.f));
+    p_model->setNodeData(node_id2,
+                         QtNodes::NodeRole::Position,
+                         QPointF(HSD_NODE_SPACING, 0.f));
+
+    MakeBinary *p_node = p_model->delegateModel<MakeBinary>(node_id2);
+    p_node->attr.at("threshold")->get_ref<FloatAttribute>()->value = 0.5f;
+    p_node->compute();
+
+    return node_id2;
+  }
+  //
   else if (node_type == "Noise")
   {
     float           pos_x = 0.f;
--- Hesiod/src/model/model_registry.cpp
+++ Hesiod/src/model/model_registry.cpp
@@ -38,6 +38,7 @@ std::shared_ptr<QtNodes::NodeDelegateModelRegistry> register_data_models(
   HSD_REGISTER_NODE(hesiod::HeightMapToMask, "Converter");
   HSD_REGISTER_NODE(hesiod::Laplace, "Filter/Smoothing");
   HSD_REGISTER_NODE(hesiod::Lerp, "Operator/Blend");
+  HSD_REGISTER_NODE(hesiod::MakeBinary, "Operator/Morphology");
   HSD_REGISTER_NODE(hesiod::Median3x3, "Filter/Smoothing");
   HSD_REGISTER_NODE(hesiod::MixTexture, "Texture");
   HSD_REGISTER_NODE(hesiod::Noise, "Primitive/Coherent Noise");
