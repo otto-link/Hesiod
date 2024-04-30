5d83fc14a0d1c509dd1cd2dcc6a0b215b5b429ff
Otto Link <otto.link.bv@gmail.com>
2024-04-30 20:51:09 +0200

    Add SelecPulse node

--- Hesiod/src/model/model_registry.cpp
+++ Hesiod/src/model/model_registry.cpp
@@ -66,6 +66,7 @@ std::shared_ptr<QtNodes::NodeDelegateModelRegistry> register_data_models(
   HSD_REGISTER_NODE(hesiod::SelectCavities, "Mask/Selector");
   HSD_REGISTER_NODE(hesiod::SelectGt, "Mask/Selector");
   HSD_REGISTER_NODE(hesiod::SelectInterval, "Mask/Selector");
+  HSD_REGISTER_NODE(hesiod::SelectPulse, "Mask/Selector");
   HSD_REGISTER_NODE(hesiod::SetAlpha, "Texture");
   HSD_REGISTER_NODE(hesiod::SharpenCone, "Filter/Smoothing");
   HSD_REGISTER_NODE(hesiod::SmoothCpulse, "Filter/Smoothing");
