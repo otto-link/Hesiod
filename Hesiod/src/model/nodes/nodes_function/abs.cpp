5301982e4e5e63cc7555dc42c147c63616522fa2
Otto Link <otto.link.bv@gmail.com>
2025-01-14 22:21:29 +0100

    Update transform mode for Abs

--- Hesiod/src/model/nodes/nodes_function/abs.cpp
+++ Hesiod/src/model/nodes/nodes_function/abs.cpp
@@ -40,10 +40,15 @@ void compute_abs_node(BaseNode *p_node)
   {
     hmap::Heightmap *p_out = p_node->get_value_ref<hmap::Heightmap>("output");
 
-    hmap::transform(*p_out,
-                    *p_in,
-                    [p_node](hmap::Array &out, hmap::Array &in)
-                    { out = hmap::abs(in - GET("vshift", FloatAttribute)); });
+    hmap::transform(
+        {p_out, p_in},
+        [p_node](std::vector<hmap::Array *> p_arrays, hmap::Vec2<int>, hmap::Vec4<float>)
+        {
+          hmap::Array *pa_out = p_arrays[0];
+          hmap::Array *pa_in = p_arrays[1];
+          *pa_out = hmap::abs(*pa_in - GET("vshift", FloatAttribute));
+        },
+        p_node->get_config_ref()->hmap_transform_mode_cpu);
   }
 
   Q_EMIT p_node->compute_finished(p_node->get_id());
