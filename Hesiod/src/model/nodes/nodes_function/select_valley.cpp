d8117013dbb9597bfe72a62b1b32622e3088381c
Otto Link <otto.link.bv@gmail.com>
2025-01-28 15:37:25 +0100

    Add GPU for SelectValley node

--- Hesiod/src/model/nodes/nodes_function/select_valley.cpp
+++ Hesiod/src/model/nodes/nodes_function/select_valley.cpp
@@ -1,6 +1,7 @@
 /* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
  * Public License. The full license is in the file LICENSE, distributed with
  * this software. */
+#include "highmap/opencl/gpu_opencl.hpp"
 #include "highmap/selector.hpp"
 
 #include "attributes.hpp"
@@ -31,10 +32,16 @@ void setup_select_valley_node(BaseNode *p_node)
                                    0.f,
                                    0.2f,
                                    "smoothing_radius");
+  p_node->add_attr<BoolAttribute>("GPU", HSD_DEFAULT_GPU_MODE, "GPU");
 
   // attribute(s) order
-  p_node->set_attr_ordered_key(
-      {"radius", "_SEPARATOR_", "inverse", "smoothing", "smoothing_radius"});
+  p_node->set_attr_ordered_key({"radius",
+                                "_SEPARATOR_",
+                                "inverse",
+                                "smoothing",
+                                "smoothing_radius",
+                                "_SEPARATOR_",
+                                "GPU"});
 }
 
 void compute_select_valley_node(BaseNode *p_node)
@@ -51,10 +58,34 @@ void compute_select_valley_node(BaseNode *p_node)
 
     int ir = std::max(1, (int)(GET("radius", FloatAttribute) * p_out->shape.x));
 
-    hmap::transform(*p_out,
-                    *p_in,
-                    [p_node, &ir](hmap::Array &array)
-                    { return hmap::select_valley(array, ir); });
+    if (GET("GPU", BoolAttribute))
+    {
+      hmap::transform(
+          {p_out, p_in},
+          [p_node,
+           ir](std::vector<hmap::Array *> p_arrays, hmap::Vec2<int>, hmap::Vec4<float>)
+          {
+            hmap::Array *pa_out = p_arrays[0];
+            hmap::Array *pa_in = p_arrays[1];
+
+            *pa_out = hmap::gpu::select_valley(*pa_in, ir);
+          },
+          p_node->get_config_ref()->hmap_transform_mode_gpu);
+    }
+    else
+    {
+      hmap::transform(
+          {p_out, p_in},
+          [p_node,
+           ir](std::vector<hmap::Array *> p_arrays, hmap::Vec2<int>, hmap::Vec4<float>)
+          {
+            hmap::Array *pa_out = p_arrays[0];
+            hmap::Array *pa_in = p_arrays[1];
+
+            *pa_out = hmap::select_valley(*pa_in, ir);
+          },
+          p_node->get_config_ref()->hmap_transform_mode_cpu);
+    }
 
     p_out->smooth_overlap_buffers();
 
