a023c4583f3f82eebfd15487c8ee94770d457b35
Otto Link <otto.link.bv@gmail.com>
2025-04-12 12:30:06 +0200

    Split source files

---
+++ Hesiod/src/model/nodes/post_process.cpp
@@ -0,0 +1,87 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+#include "highmap/filters.hpp"
+#include "highmap/heightmap.hpp"
+#include "highmap/opencl/gpu_opencl.hpp"
+#include "highmap/range.hpp"
+
+#include "hesiod/model/nodes/base_node.hpp"
+
+namespace hesiod
+{
+
+void post_apply_enveloppe(BaseNode *p_node, hmap::Heightmap &h, hmap::Heightmap *p_env)
+{
+  if (p_env)
+  {
+    float hmin = h.min();
+
+    hmap::transform(
+        {&h, p_env},
+        [hmin](std::vector<hmap::Array *> p_arrays)
+        {
+          hmap::Array *pa_out = p_arrays[0];
+          hmap::Array *pa_env = p_arrays[1];
+
+          *pa_out -= hmin;
+          *pa_out *= *pa_env;
+        },
+        p_node->get_config_ref()->hmap_transform_mode_cpu);
+  }
+}
+
+void post_process_heightmap(BaseNode         *p_node,
+                            hmap::Heightmap  &h,
+                            bool              inverse,
+                            bool              smoothing,
+                            float             smoothing_radius,
+                            bool              saturate,
+                            hmap::Vec2<float> saturate_range,
+                            float             saturate_k,
+                            bool              remap,
+                            hmap::Vec2<float> remap_range)
+{
+  if (inverse)
+    h.inverse();
+
+  if (smoothing)
+  {
+    int ir = std::max(1, (int)(smoothing_radius * h.shape.x));
+
+    hmap::transform(
+        {&h},
+        [&ir](std::vector<hmap::Array *> p_arrays)
+        {
+          hmap::Array *pa_out = p_arrays[0];
+          return hmap::gpu::smooth_cpulse(*pa_out, ir);
+        },
+        p_node->get_config_ref()->hmap_transform_mode_gpu);
+
+    h.smooth_overlap_buffers();
+  }
+
+  if (saturate)
+  {
+    float hmin = h.min();
+    float hmax = h.max();
+
+    // node parameters are assumed normalized and thus in [0, 1],
+    // they need to be rescaled
+    float smin_n = hmin + saturate_range.x * (hmax - hmin);
+    float smax_n = hmax - (1.f - saturate_range.y) * (hmax - hmin);
+    float k_n = std::max(1e-6f, saturate_k * (hmax - hmin));
+
+    hmap::transform(h,
+                    [&smin_n, &smax_n, &k_n](hmap::Array &array)
+                    { hmap::clamp_smooth(array, smin_n, smax_n, k_n); });
+
+    // keep original amplitude
+    h.remap(hmin, hmax);
+  }
+
+  if (remap)
+    h.remap(remap_range.x, remap_range.y);
+}
+
+} // namespace hesiod
--- Hesiod/src/model/utils.cpp
+++ Hesiod/src/model/utils.cpp
@@ -5,12 +5,9 @@
 #include <fstream>
 #include <iomanip>
 
-#include "highmap/filters.hpp"
-#include "highmap/heightmap.hpp"
-#include "highmap/opencl/gpu_opencl.hpp"
-#include "highmap/range.hpp"
+#include "nlohmann/json.hpp"
 
-#include "hesiod/model/nodes/base_node.hpp"
+#include "hesiod/logger.hpp"
 
 namespace hesiod
 {
@@ -50,79 +47,6 @@ void json_to_file(const nlohmann::json &json, const std::string &fname)
   }
 }
 
-void post_apply_enveloppe(BaseNode *p_node, hmap::Heightmap &h, hmap::Heightmap *p_env)
-{
-  if (p_env)
-  {
-    float hmin = h.min();
-
-    hmap::transform(
-        {&h, p_env},
-        [hmin](std::vector<hmap::Array *> p_arrays)
-        {
-          hmap::Array *pa_out = p_arrays[0];
-          hmap::Array *pa_env = p_arrays[1];
-
-          *pa_out -= hmin;
-          *pa_out *= *pa_env;
-        },
-        p_node->get_config_ref()->hmap_transform_mode_cpu);
-  }
-}
-
-void post_process_heightmap(BaseNode         *p_node,
-                            hmap::Heightmap  &h,
-                            bool              inverse,
-                            bool              smoothing,
-                            float             smoothing_radius,
-                            bool              saturate,
-                            hmap::Vec2<float> saturate_range,
-                            float             saturate_k,
-                            bool              remap,
-                            hmap::Vec2<float> remap_range)
-{
-  if (inverse)
-    h.inverse();
-
-  if (smoothing)
-  {
-    int ir = std::max(1, (int)(smoothing_radius * h.shape.x));
-
-    hmap::transform(
-        {&h},
-        [&ir](std::vector<hmap::Array *> p_arrays)
-        {
-          hmap::Array *pa_out = p_arrays[0];
-          return hmap::gpu::smooth_cpulse(*pa_out, ir);
-        },
-        p_node->get_config_ref()->hmap_transform_mode_gpu);
-
-    h.smooth_overlap_buffers();
-  }
-
-  if (saturate)
-  {
-    float hmin = h.min();
-    float hmax = h.max();
-
-    // node parameters are assumed normalized and thus in [0, 1],
-    // they need to be rescaled
-    float smin_n = hmin + saturate_range.x * (hmax - hmin);
-    float smax_n = hmax - (1.f - saturate_range.y) * (hmax - hmin);
-    float k_n = std::max(1e-6f, saturate_k * (hmax - hmin));
-
-    hmap::transform(h,
-                    [&smin_n, &smax_n, &k_n](hmap::Array &array)
-                    { hmap::clamp_smooth(array, smin_n, smax_n, k_n); });
-
-    // keep original amplitude
-    h.remap(hmin, hmax);
-  }
-
-  if (remap)
-    h.remap(remap_range.x, remap_range.y);
-}
-
 std::vector<std::string> split_string(const std::string &string, char delimiter)
 {
   std::vector<std::string> result;
@@ -140,7 +64,7 @@ std::string time_stamp()
   auto              now = std::chrono::system_clock::now();
   std::time_t       now_c = std::chrono::system_clock::to_time_t(now);
   std::stringstream time_stream;
-  time_stream << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S");
+  time_stream << std::put_time(std::localtime(&now_c), "%Y-%m-%d_%H:%M:%S");
   return time_stream.str();
 }
 
