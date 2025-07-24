f4fd6e87ab4be38eb620719fbb963f526203e9de
Otto Link <otto.link.bv@gmail.com>
2025-07-24 15:50:56 +0200

    Move BaseNode specialized GUI to dedicated header

---
+++ Hesiod/include/hesiod/model/nodes/base_node_gui.hpp
@@ -0,0 +1,14 @@
+
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General Public
+   License. The full license is in the file LICENSE, distributed with this software. */
+#pragma once
+
+namespace hesiod
+{
+
+class BaseNode; // forward
+
+// specialized GUI to add on the node face in the node editor
+void add_export_button(BaseNode *p_node);
+
+} // namespace hesiod
