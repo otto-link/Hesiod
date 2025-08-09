f13edaa2d13f91c7ab0f11ad4a3011b74403c15f
Otto Link <otto.link.bv@gmail.com>
2025-08-09 23:19:27 +0200

    Update HighMap, update cloud generation nodes

--- Hesiod/src/model/nodes/nodes_function/cloud_random.cpp
+++ Hesiod/src/model/nodes/nodes_function/cloud_random.cpp
@@ -23,10 +23,14 @@ void setup_cloud_random_node(BaseNode *p_node)
   // attribute(s)
   ADD_ATTR(IntAttribute, "npoints", 50, 1, INT_MAX);
   ADD_ATTR(SeedAttribute, "seed");
+  ADD_ATTR(EnumAttribute,
+           "method",
+           hmap::point_sampling_method_as_string,
+           "Latin Hypercube Sampling");
   ADD_ATTR(RangeAttribute, "remap");
 
   // attribute(s) order
-  p_node->set_attr_ordered_key({"npoints", "seed", "_SEPARATOR_", "remap"});
+  p_node->set_attr_ordered_key({"npoints", "seed", "method", "_SEPARATOR_", "remap"});
 }
 
 void compute_cloud_random_node(BaseNode *p_node)
@@ -37,7 +41,9 @@ void compute_cloud_random_node(BaseNode *p_node)
 
   hmap::Cloud *p_out = p_node->get_value_ref<hmap::Cloud>("cloud");
 
-  *p_out = hmap::Cloud(GET("npoints", IntAttribute), GET("seed", SeedAttribute));
+  *p_out = hmap::random_cloud(GET("npoints", IntAttribute),
+                              GET("seed", SeedAttribute),
+                              (hmap::PointSamplingMethod)GET("method", EnumAttribute));
 
   if (GET_MEMBER("remap", RangeAttribute, is_active))
     p_out->remap_values(GET("remap", RangeAttribute)[0], GET("remap", RangeAttribute)[1]);
