0fda21f220d466c11a43ff431402fd39cbd53969
Otto Link <otto.link.bv@gmail.com>
2025-09-26 08:57:43 +0200

    Add TextureSplitChannels and TextureToHeightmap nodes (#355 #356)

---
+++ Hesiod/src/model/nodes/nodes_function/texture_split_channels.cpp
@@ -0,0 +1,59 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+#include "highmap/gradient.hpp"
+#include "highmap/range.hpp"
+
+#include "attributes.hpp"
+
+#include "hesiod/logger.hpp"
+#include "hesiod/model/nodes/base_node.hpp"
+#include "hesiod/model/nodes/base_node_gui.hpp"
+#include "hesiod/model/nodes/post_process.hpp"
+
+using namespace attr;
+
+namespace hesiod
+{
+
+void setup_texture_split_channels_node(BaseNode *p_node)
+{
+  LOG->trace("setup node {}", p_node->get_label());
+
+  // port(s)
+  p_node->add_port<hmap::HeightmapRGBA>(gnode::PortType::IN, "texture");
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::OUT, "R", CONFIG);
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::OUT, "G", CONFIG);
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::OUT, "B", CONFIG);
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::OUT, "A", CONFIG);
+
+  // attribute(s)
+
+  // attribute(s) order
+}
+
+void compute_texture_split_channels_node(BaseNode *p_node)
+{
+  Q_EMIT p_node->compute_started(p_node->get_id());
+
+  LOG->trace("computing node [{}]/[{}]", p_node->get_label(), p_node->get_id());
+
+  hmap::HeightmapRGBA *p_tex = p_node->get_value_ref<hmap::HeightmapRGBA>("texture");
+
+  if (p_tex)
+  {
+    hmap::Heightmap *p_r = p_node->get_value_ref<hmap::Heightmap>("R");
+    hmap::Heightmap *p_g = p_node->get_value_ref<hmap::Heightmap>("G");
+    hmap::Heightmap *p_b = p_node->get_value_ref<hmap::Heightmap>("B");
+    hmap::Heightmap *p_a = p_node->get_value_ref<hmap::Heightmap>("A");
+
+    *p_r = p_tex->rgba[0];
+    *p_g = p_tex->rgba[1];
+    *p_b = p_tex->rgba[2];
+    *p_a = p_tex->rgba[3];
+  }
+
+  Q_EMIT p_node->compute_finished(p_node->get_id());
+}
+
+} // namespace hesiod
---
+++ docs/node_reference/nodes/TextureSplitChannels.md
@@ -0,0 +1,35 @@
+
+TextureSplitChannels Node
+=========================
+
+
+No description available
+
+
+
+![img](../../images/nodes/TextureSplitChannels_settings.png)
+
+
+# Category
+
+
+Texture
+# Inputs
+
+|Name|Type|Description|
+| :--- | :--- | :--- |
+|texture|HeightmapRGBA|No description|
+
+# Outputs
+
+|Name|Type|Description|
+| :--- | :--- | :--- |
+|A|Heightmap|No description|
+|B|Heightmap|No description|
+|G|Heightmap|No description|
+|R|Heightmap|No description|
+
+# Example
+
+
+No example available.
