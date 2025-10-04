b5ba666a5a846074d94b14886988e1a83a75c35d
Otto Link <otto.link.bv@gmail.com>
2025-10-04 21:04:57 +0200

    Add ConeSigmoid node

---
+++ Hesiod/src/model/nodes/nodes_function/cone_sigmoid.cpp
@@ -0,0 +1,80 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+
+#include "highmap/heightmap.hpp"
+#include "highmap/primitives.hpp"
+
+#include "attributes.hpp"
+
+#include "hesiod/logger.hpp"
+#include "hesiod/model/nodes/base_node.hpp"
+#include "hesiod/model/nodes/post_process.hpp"
+
+using namespace attr;
+
+namespace hesiod
+{
+
+void setup_cone_sigmoid_node(BaseNode *p_node)
+{
+  Logger::log()->trace("setup node {}", p_node->get_label());
+
+  // port(s)
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::IN, "dx");
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::IN, "dy");
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::IN, "envelope");
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::OUT, "output", CONFIG);
+
+  // attribute(s)
+  ADD_ATTR(FloatAttribute, "radius", 0.5f, 0.01f, FLT_MAX);
+  ADD_ATTR(FloatAttribute, "alpha", 0.8f, 0.01f, 4.f);
+  ADD_ATTR(Vec2FloatAttribute, "center");
+
+  // attribute(s) order
+  p_node->set_attr_ordered_key({"radius", "alpha", "center"});
+
+  setup_post_process_heightmap_attributes(p_node);
+}
+
+void compute_cone_sigmoid_node(BaseNode *p_node)
+{
+
+  Q_EMIT p_node->compute_started(p_node->get_id());
+
+  Logger::log()->trace("computing node [{}]/[{}]", p_node->get_label(), p_node->get_id());
+
+  // base noise function
+  hmap::Heightmap *p_dx = p_node->get_value_ref<hmap::Heightmap>("dx");
+  hmap::Heightmap *p_dy = p_node->get_value_ref<hmap::Heightmap>("dy");
+  hmap::Heightmap *p_env = p_node->get_value_ref<hmap::Heightmap>("envelope");
+  hmap::Heightmap *p_out = p_node->get_value_ref<hmap::Heightmap>("output");
+
+  hmap::transform(
+      {p_out, p_dx, p_dy},
+      [p_node](std::vector<hmap::Array *> p_arrays,
+               hmap::Vec2<int>            shape,
+               hmap::Vec4<float>          bbox)
+      {
+        hmap::Array *pa_out = p_arrays[0];
+        hmap::Array *pa_dx = p_arrays[1];
+        hmap::Array *pa_dy = p_arrays[2];
+
+        *pa_out = hmap::cone_sigmoid(shape,
+                                     GET("alpha", FloatAttribute),
+                                     GET("radius", FloatAttribute),
+                                     GET("center", Vec2FloatAttribute),
+                                     pa_dx,
+                                     pa_dy,
+                                     bbox);
+      },
+      p_node->get_config_ref()->hmap_transform_mode_cpu);
+
+  // post-process
+  post_apply_enveloppe(p_node, *p_out, p_env);
+  post_process_heightmap(p_node, *p_out);
+
+  Q_EMIT p_node->compute_finished(p_node->get_id());
+}
+
+} // namespace hesiod
