183c6fde770be0b81aafbd03730b574df633ced5
Otto Link <otto.link.bv@gmail.com>
2025-10-10 20:44:37 +0200

    Update Attributes (nex ColorPicker UI)

--- Hesiod/include/hesiod/model/graph_node.hpp
+++ Hesiod/include/hesiod/model/graph_node.hpp
@@ -1,13 +1,5 @@
 /* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General Public
    License. The full license is in the file LICENSE, distributed with this software. */
-
-/**
- * @file graph_node.hpp
- * @author  Otto Link (otto.link.bv@gmail.com)
- * @brief
- *
- * @copyright Copyright (c) 2023
- */
 #pragma once
 #include <QObject>
 
