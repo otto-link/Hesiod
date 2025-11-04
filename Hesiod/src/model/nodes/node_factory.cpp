247959b00adb1812a47b7a0cf4e1a2f2835cd03d
Otto Link <otto.link.bv@gmail.com>
2025-11-04 23:38:38 +0100

    Add SelectSoilFlow node

--- Hesiod/src/model/nodes/node_factory.cpp
+++ Hesiod/src/model/nodes/node_factory.cpp
@@ -337,6 +337,7 @@ std::map<std::string, std::string> get_node_inventory()
       {"SelectPulse", "Mask/Selector"},
       {"SelectRivers", "Mask/Selector"},
       {"SelectSlope", "Mask/Selector"},
+      {"SelectSoilFlow", "Mask/ForTexturing"},
       {"SelectSoilWeathered", "Mask/ForTexturing"},
       {"SelectTransitions", "Mask/Selector"},
       {"SelectValley", "Mask/Selector"},
@@ -633,6 +634,7 @@ std::shared_ptr<gnode::Node> node_factory(const std::string           &node_type
     SETUP_NODE(SelectPulse, select_pulse);
     SETUP_NODE(SelectRivers, select_rivers);
     SETUP_NODE(SelectSlope, select_slope);
+    SETUP_NODE(SelectSoilFlow, select_soil_flow);
     SETUP_NODE(SelectSoilWeathered, select_soil_weathered);
     SETUP_NODE(SelectTransitions, select_transitions);
     SETUP_NODE(SelectValley, select_valley);
