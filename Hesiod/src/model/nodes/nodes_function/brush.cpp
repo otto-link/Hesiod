84a1811ae01d37029a720ef8e4c5101670c3271c
Otto Link <otto.link.bv@gmail.com>
2025-11-14 21:22:57 +0100

    Add background image optional input for Brush node, fix output data orientation

--- Hesiod/src/model/nodes/nodes_function/brush.cpp
+++ Hesiod/src/model/nodes/nodes_function/brush.cpp
@@ -1,6 +1,7 @@
 /* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
  * Public License. The full license is in the file LICENSE, distributed with
  * this software. */
+#include "highmap/colorize.hpp"
 #include "highmap/heightmap.hpp"
 
 #include "attributes.hpp"
@@ -19,6 +20,7 @@ void setup_brush_node(BaseNode *p_node)
   Logger::log()->trace("setup node {}", p_node->get_label());
 
   // port(s)
+  p_node->add_port<hmap::Heightmap>(gnode::PortType::IN, "background");
   p_node->add_port<hmap::Heightmap>(gnode::PortType::OUT, "out", CONFIG);
 
   // attribute(s)
@@ -28,6 +30,35 @@ void setup_brush_node(BaseNode *p_node)
   p_node->set_attr_ordered_key({"hmap"});
 
   setup_post_process_heightmap_attributes(p_node);
+
+  // set background image callback
+  std::string port_id = "background";
+
+  auto lambda = [p_node, port_id]()
+  {
+    hmap::Heightmap *p_in = p_node->get_value_ref<hmap::Heightmap>(port_id);
+
+    if (!p_in)
+      return QImage();
+
+    // generate a preview of the heightmap
+    hmap::Vec2<int> shape_preview = hmap::Vec2<int>(256, 256);
+    hmap::Array     array = p_in->to_array(shape_preview);
+
+    std::vector<uint8_t> img(shape_preview.x * shape_preview.y);
+
+    img = hmap::colorize(array, array.min(), array.max(), hmap::Cmap::MAGMA, false)
+              .to_img_8bit();
+
+    QImage tmp_image = QImage(img.data(),
+                              shape_preview.x,
+                              shape_preview.y,
+                              QImage::Format_RGB888);
+    return tmp_image.copy();
+  };
+
+  // assign function to attr
+  GET_REF("hmap", ArrayAttribute)->set_background_image_fct(lambda);
 }
 
 void compute_brush_node(BaseNode *p_node)
