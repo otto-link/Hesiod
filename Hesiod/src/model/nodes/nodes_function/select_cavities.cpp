88fa297ff93550d2bdaaea48be0f9403360db622
Otto Link <otto.link.bv@gmail.com>
2025-12-27 10:10:51 +0100

    Add post_filter and GPU version for SelectCavities (#488, #489)

--- Hesiod/src/model/nodes/nodes_function/select_cavities.cpp
+++ Hesiod/src/model/nodes/nodes_function/select_cavities.cpp
@@ -1,6 +1,7 @@
 /* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
  * Public License. The full license is in the file LICENSE, distributed with
  * this software. */
+#include "highmap/filters.hpp"
 #include "highmap/selector.hpp"
 
 #include "attributes.hpp"
@@ -23,11 +24,16 @@ void setup_select_cavities_node(BaseNode &node)
   node.add_port<hmap::Heightmap>(gnode::PortType::OUT, "output", CONFIG(node));
 
   // attribute(s)
-  node.add_attr<FloatAttribute>("radius", "radius", 0.05f, 0.001f, 0.2f);
-  node.add_attr<BoolAttribute>("concave", "concave", true);
+  node.add_attr<FloatAttribute>("radius", "Selection Scale", 0.05f, 0.001f, 0.2f);
+  node.add_attr<BoolAttribute>("concave", "", "Bumps", "Holes", false);
+  node.add_attr<BoolAttribute>("post_filter", "Enable Smoothing", true);
 
   // attribute(s) order
-  node.set_attr_ordered_key({"radius", "concave"});
+  node.set_attr_ordered_key({"_GROUPBOX_BEGIN_Main Parameters",
+                             "radius",
+                             "concave",
+                             "post_filter",
+                             "_GROUPBOX_END_"});
 
   setup_post_process_heightmap_attributes(node,
                                           {.add_mix = true, .remap_active_state = true});
@@ -45,14 +51,21 @@ void compute_select_cavities_node(BaseNode &node)
 
     int ir = std::max(1, (int)(node.get_attr<FloatAttribute>("radius") * p_out->shape.x));
 
-    hmap::transform(*p_out,
-                    *p_in,
-                    [&node, &ir](hmap::Array &array) {
-                      return hmap::select_cavities(
-                          array,
-                          ir,
-                          node.get_attr<BoolAttribute>("concave"));
-                    });
+    hmap::transform(
+        {p_out, p_in},
+        [&node, ir](std::vector<hmap::Array *> p_arrays)
+        {
+          hmap::Array *pa_out = p_arrays[0];
+          hmap::Array *pa_in = p_arrays[1];
+
+          *pa_out = hmap::gpu::select_cavities(*pa_in,
+                                               ir,
+                                               node.get_attr<BoolAttribute>("concave"));
+
+          if (node.get_attr<BoolAttribute>("post_filter"))
+            hmap::laplace(*pa_out);
+        },
+        node.get_config_ref()->hmap_transform_mode_gpu);
 
     p_out->smooth_overlap_buffers();
 
