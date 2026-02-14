ed9034ddb9c931850cd9de7c229c58fd96b97295
Otto Link <otto.link.bv@gmail.com>
2026-02-14 21:06:37 +0100

    Fix Warp node, wrong routing between inputs within the node compute caused app crash

--- Hesiod/src/model/nodes/nodes_function/warp.cpp
+++ Hesiod/src/model/nodes/nodes_function/warp.cpp
@@ -15,60 +15,79 @@ using namespace attr;
 namespace hesiod
 {
 
+// -----------------------------------------------------------------------------
+// Ports & Attributes
+// -----------------------------------------------------------------------------
+
+constexpr const char *P_INPUT = "input";
+constexpr const char *P_DX = "dx";
+constexpr const char *P_DY = "dy";
+constexpr const char *P_OUTPUT = "output";
+
+constexpr const char *A_SCALING_X = "scaling.x";
+constexpr const char *A_SCALING_Y = "scaling.y";
+
+// -----------------------------------------------------------------------------
+// Setup
+// -----------------------------------------------------------------------------
+
 void setup_warp_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
   // port(s)
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "input");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "dx");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "dy");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, "output", CONFIG(node));
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_INPUT);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_DX);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_DY);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUTPUT, CONFIG(node));
 
   // attribute(s)
-  node.add_attr<FloatAttribute>("scaling.x", "scaling.x", 1.f, -2.f, 2.f);
-  node.add_attr<FloatAttribute>("scaling.y", "scaling.y", 1.f, -2.f, 2.f);
+  node.add_attr<FloatAttribute>(A_SCALING_X, "scaling.x", 1.f, -2.f, 2.f);
+  node.add_attr<FloatAttribute>(A_SCALING_Y, "scaling.y", 1.f, -2.f, 2.f);
 
   // attribute(s) order
-  node.set_attr_ordered_key({"scaling.x", "scaling.y"});
+  node.set_attr_ordered_key({A_SCALING_X, A_SCALING_Y});
 
   setup_post_process_heightmap_attributes(node,
                                           {.add_mix = true, .remap_active_state = false});
 }
 
+// -----------------------------------------------------------------------------
+// Compute
+// -----------------------------------------------------------------------------
+
 void compute_warp_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
-  hmap::VirtualArray *p_in = node.get_value_ref<hmap::VirtualArray>("input");
+  auto *p_in = node.get_value_ref<hmap::VirtualArray>(P_INPUT);
+  auto *p_dx = node.get_value_ref<hmap::VirtualArray>(P_DX);
+  auto *p_dy = node.get_value_ref<hmap::VirtualArray>(P_DY);
+  auto *p_out = node.get_value_ref<hmap::VirtualArray>(P_OUTPUT);
 
-  if (p_in)
-  {
-    hmap::VirtualArray *p_dx = node.get_value_ref<hmap::VirtualArray>("dx");
-    hmap::VirtualArray *p_dy = node.get_value_ref<hmap::VirtualArray>("dy");
-    hmap::VirtualArray *p_out = node.get_value_ref<hmap::VirtualArray>("output");
+  if (!p_in)
+    return;
 
-    float sx = node.get_attr<FloatAttribute>("scaling.x");
-    float sy = node.get_attr<FloatAttribute>("scaling.y");
+  float sx = node.get_attr<FloatAttribute>(A_SCALING_X);
+  float sy = node.get_attr<FloatAttribute>(A_SCALING_Y);
 
-    hmap::for_each_tile(
-        {p_out, p_in, p_dx, p_dy},
-        [sx, sy](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
-        {
-          hmap::Array *pa_out = p_arrays[0];
-          hmap::Array *pa_in = p_arrays[1];
-          hmap::Array dx = p_arrays[2] ? sx * (*p_arrays[1]) : hmap::Array(pa_out->shape);
-          hmap::Array dy = p_arrays[3] ? sy * (*p_arrays[2]) : hmap::Array(pa_out->shape);
+  hmap::for_each_tile(
+      {p_out, p_in, p_dx, p_dy},
+      [sx, sy](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
+      {
+        auto [pa_out, pa_in, pa_dx, pa_dy] = unpack<4>(p_arrays);
 
-          *pa_out = *pa_in;
+        hmap::Array dx = pa_dx ? sx * (*pa_dx) : hmap::Array(pa_out->shape);
+        hmap::Array dy = pa_dy ? sy * (*pa_dy) : hmap::Array(pa_out->shape);
 
-          hmap::gpu::warp(*pa_out, &dx, &dy);
-        },
-        node.cfg().cm_gpu);
+        *pa_out = *pa_in;
 
-    // post-process
-    post_process_heightmap(node, *p_out, p_in);
-  }
+        hmap::gpu::warp(*pa_out, &dx, &dy);
+      },
+      node.cfg().cm_gpu);
+
+  // post-process
+  post_process_heightmap(node, *p_out, p_in);
 }
 
 } // namespace hesiod
