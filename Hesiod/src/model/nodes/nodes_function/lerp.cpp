9aad5420fe433dd12d4d24494c8f433a055163af
Otto Link <otto.link.bv@gmail.com>
2026-04-26 23:18:18 +0200

    Update sources of Lerp node (same functionality)

--- Hesiod/src/model/nodes/nodes_function/lerp.cpp
+++ Hesiod/src/model/nodes/nodes_function/lerp.cpp
@@ -13,6 +13,7 @@ using namespace attr;
 
 namespace hesiod
 {
+
 // -----------------------------------------------------------------------------
 // Ports & Attributes
 // -----------------------------------------------------------------------------
@@ -33,18 +34,25 @@ void setup_lerp_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
-  // port(s)
+  // --- Ports
+
   node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_A);
   node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_B);
   node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_T);
   node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUT, CONFIG(node));
 
-  // attribute(s)
+  // --- Attributes
+
   node.add_attr<FloatAttribute>(A_T, "Blend Factor 't' (a -> b)", 0.5f, 0.f, 1.f);
   node.add_attr<BoolAttribute>(A_SWAP_INPUTS, "Swap Inputs", false);
 
-  // attribute(s) order
-  node.set_attr_ordered_key({A_T, A_SWAP_INPUTS});
+  // --- Attribute(s) order
+
+  node.set_attr_ordered_key(
+      {"_GROUPBOX_BEGIN_Main Parameters", A_T, A_SWAP_INPUTS, "_GROUPBOX_END_"});
+
+  setup_post_process_heightmap_attributes(node,
+                                          {.add_mix = true, .remap_active_state = false});
 }
 
 // -----------------------------------------------------------------------------
@@ -55,6 +63,8 @@ void compute_lerp_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
+  // --- Inputs / Outputs
+
   auto *p_a = node.get_value_ref<hmap::VirtualArray>(P_A);
   auto *p_b = node.get_value_ref<hmap::VirtualArray>(P_B);
   auto *p_t = node.get_value_ref<hmap::VirtualArray>(P_T);
@@ -63,27 +73,38 @@ void compute_lerp_node(BaseNode &node)
   if (!p_a || !p_b)
     return;
 
-  // adjust inputs
-  if (node.get_attr<BoolAttribute>(A_SWAP_INPUTS))
+  // --- Params
+
+  // clang-format off
+  const auto t_attr      = node.get_attr<FloatAttribute>(A_T);
+  const auto swap_inputs = node.get_attr<BoolAttribute>(A_SWAP_INPUTS);
+  // clang-format on
+
+  if (swap_inputs)
     std::swap(p_a, p_b);
 
-  // compute output
+  // --- Compute
+
   hmap::for_each_tile(
       {p_a, p_b, p_t},
       {p_out},
-      [&node](std::vector<const hmap::Array *> p_arrays_in,
-              std::vector<hmap::Array *>       p_arrays_out,
-              const hmap::TileRegion &)
+      [&](std::vector<const hmap::Array *> in,
+          std::vector<hmap::Array *>       out,
+          const hmap::TileRegion &)
       {
-        auto [pa_a, pa_b, pa_t] = unpack<3>(p_arrays_in);
-        auto [pa_out] = unpack<1>(p_arrays_out);
+        auto [pa_a, pa_b, pa_t] = unpack<3>(in);
+        auto [pa_out] = unpack<1>(out);
 
         if (pa_t)
           *pa_out = hmap::lerp(*pa_a, *pa_b, *pa_t);
         else
-          *pa_out = hmap::lerp(*pa_a, *pa_b, node.get_attr<FloatAttribute>(P_T));
+          *pa_out = hmap::lerp(*pa_a, *pa_b, t_attr);
       },
       node.cfg().cm_cpu);
+
+  // --- Post-process
+
+  post_process_heightmap(node, *p_out, p_a);
 }
 
 } // namespace hesiod
