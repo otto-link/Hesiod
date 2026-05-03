969f3835f0768a0fcd64f8e4c538ed147ef7d869
Otto Link <otto.link.bv@gmail.com>
2026-05-03 12:35:25 +0200

    Fix Clamp node

--- Hesiod/src/model/nodes/nodes_function/clamp.cpp
+++ Hesiod/src/model/nodes/nodes_function/clamp.cpp
@@ -14,121 +14,119 @@ using namespace attr;
 namespace hesiod
 {
 
+// -----------------------------------------------------------------------------
+// Ports & Attributes
+// -----------------------------------------------------------------------------
+
+constexpr const char *P_IN = "input";
+constexpr const char *P_OUT = "output";
+
+constexpr const char *A_CLAMP = "clamp";
+constexpr const char *A_SMOOTH_MIN = "smooth_min";
+constexpr const char *A_K_MIN = "k_min";
+constexpr const char *A_SMOOTH_MAX = "smooth_max";
+constexpr const char *A_K_MAX = "k_max";
+constexpr const char *A_REMAP = "remap";
+
+// -----------------------------------------------------------------------------
+// Setup
+// -----------------------------------------------------------------------------
+
 void setup_clamp_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
-  // port(s)
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "input");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, "output", CONFIG(node));
+  // --- Ports
 
-  // attribute(s)
-  node.add_attr<RangeAttribute>("clamp", "clamp");
-  node.add_attr<BoolAttribute>("smooth_min", "smooth_min", false);
-  node.add_attr<FloatAttribute>("k_min", "k_min", 0.05f, 0.01f, 1.f);
-  node.add_attr<BoolAttribute>("smooth_max", "smooth_max", false);
-  node.add_attr<FloatAttribute>("k_max", "k_max", 0.05f, 0.01f, 1.f);
-  node.add_attr<BoolAttribute>("remap", "remap", false);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_IN);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUT, CONFIG(node));
 
-  // link histogram for RangeAttribute
-  setup_histogram_for_range_attribute(node, "clamp", "input");
+  // --- Attributes
 
-  // attribute(s) order
-  node.set_attr_ordered_key(
-      {"clamp", "smooth_min", "k_min", "smooth_max", "k_max", "remap"});
+  node.add_attr<RangeAttribute>(A_CLAMP, "Clamp");
+  node.add_attr<BoolAttribute>(A_SMOOTH_MIN, "Smooth Min", false);
+  node.add_attr<FloatAttribute>(A_K_MIN, "K Min", 0.05f, 0.01f, 1.f);
+  node.add_attr<BoolAttribute>(A_SMOOTH_MAX, "Smooth Max", false);
+  node.add_attr<FloatAttribute>(A_K_MAX, "K Max", 0.05f, 0.01f, 1.f);
+  node.add_attr<BoolAttribute>(A_REMAP, "Remap", false);
+
+  // histogram binding
+  setup_histogram_for_range_attribute(node, A_CLAMP, P_IN);
+
+  // --- Attribute(s) order
+
+  node.set_attr_ordered_key({"_GROUPBOX_BEGIN_Main Parameters",
+                             A_CLAMP,
+                             A_SMOOTH_MIN,
+                             A_K_MIN,
+                             A_SMOOTH_MAX,
+                             A_K_MAX,
+                             A_REMAP,
+                             "_GROUPBOX_END_"});
 
   setup_post_process_heightmap_attributes(node,
                                           {.add_mix = true, .remap_active_state = false});
 }
 
+// -----------------------------------------------------------------------------
+// Compute
+// -----------------------------------------------------------------------------
+
 void compute_clamp_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
-  hmap::VirtualArray *p_in = node.get_value_ref<hmap::VirtualArray>("input");
+  // --- Inputs / Outputs
 
-  if (p_in)
-  {
-    hmap::VirtualArray *p_out = node.get_value_ref<hmap::VirtualArray>("output");
+  auto *p_in = node.get_value_ref<hmap::VirtualArray>(P_IN);
+  auto *p_out = node.get_value_ref<hmap::VirtualArray>(P_OUT);
 
-    // retrieve parameters
-    glm::vec2 crange = node.get_attr<RangeAttribute>("clamp");
-    bool      smooth_min = node.get_attr<BoolAttribute>("smooth_min");
-    bool      smooth_max = node.get_attr<BoolAttribute>("smooth_max");
-    float     k_min = node.get_attr<FloatAttribute>("k_min");
-    float     k_max = node.get_attr<FloatAttribute>("k_max");
+  if (!p_in)
+    return;
 
-    // compute
-    if (!smooth_min && !smooth_max)
-    {
-      hmap::for_each_tile(
-          {p_out, p_in},
-          [&crange](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
-          {
-            hmap::Array *pa_out = p_arrays[0];
-            hmap::Array *pa_in = p_arrays[1];
-            *pa_out = *pa_in;
-            hmap::clamp(*pa_out, crange.x, crange.y);
-          },
-          node.cfg().cm_cpu);
-    }
-    else
-    {
-      if (smooth_min)
-        hmap::for_each_tile(
-            {p_out, p_in},
-            [&crange, &k_min](std::vector<hmap::Array *> p_arrays,
-                              const hmap::TileRegion &)
-            {
-              hmap::Array *pa_out = p_arrays[0];
-              hmap::Array *pa_in = p_arrays[1];
-              *pa_out = *pa_in;
-              hmap::clamp_min_smooth(*pa_out, crange.x, k_min);
-            },
-            node.cfg().cm_cpu);
-      else
-        hmap::for_each_tile(
-            {p_out, p_in},
-            [&crange](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
-            {
-              hmap::Array *pa_out = p_arrays[0];
-              hmap::Array *pa_in = p_arrays[1];
-              *pa_out = *pa_in;
-              hmap::clamp_min(*pa_out, crange.x);
-            },
-            node.cfg().cm_cpu);
+  // --- Params
 
-      if (smooth_max)
-        hmap::for_each_tile(
-            {p_out, p_in},
-            [&crange, &k_max](std::vector<hmap::Array *> p_arrays,
-                              const hmap::TileRegion &)
-            {
-              hmap::Array *pa_out = p_arrays[0];
-              hmap::Array *pa_in = p_arrays[1];
-              *pa_out = *pa_in;
-              hmap::clamp_max_smooth(*pa_out, crange.y, k_max);
-            },
-            node.cfg().cm_cpu);
-      else
-        hmap::for_each_tile(
-            {p_out, p_in},
-            [&crange](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
-            {
-              hmap::Array *pa_out = p_arrays[0];
-              hmap::Array *pa_in = p_arrays[1];
-              *pa_out = *pa_in;
-              hmap::clamp_max(*pa_out, crange.y);
-            },
-            node.cfg().cm_cpu);
-    }
+  const auto crange = node.get_attr<RangeAttribute>(A_CLAMP);
+  const auto smooth_min = node.get_attr<BoolAttribute>(A_SMOOTH_MIN);
+  const auto smooth_max = node.get_attr<BoolAttribute>(A_SMOOTH_MAX);
+  const auto k_min = node.get_attr<FloatAttribute>(A_K_MIN);
+  const auto k_max = node.get_attr<FloatAttribute>(A_K_MAX);
+  const auto remap = node.get_attr<BoolAttribute>(A_REMAP);
 
-    if (node.get_attr<BoolAttribute>("remap"))
-      p_out->remap(0.f, 1.f, node.cfg().cm_cpu);
+  // --- Compute
 
-    // post-process
-    post_process_heightmap(node, *p_out, p_in);
-  }
+  hmap::for_each_tile(
+      {p_in},
+      {p_out},
+      [&](std::vector<const hmap::Array *> in,
+          std::vector<hmap::Array *>       out,
+          const hmap::TileRegion &)
+      {
+        auto [pa_in] = unpack<1>(in);
+        auto [pa_out] = unpack<1>(out);
+
+        *pa_out = *pa_in;
+
+        // min clamp
+        if (smooth_min)
+          hmap::clamp_min_smooth(*pa_out, crange.x, k_min);
+        else
+          hmap::clamp_min(*pa_out, crange.x);
+
+        // max clamp
+        if (smooth_max)
+          hmap::clamp_max_smooth(*pa_out, crange.y, k_max);
+        else
+          hmap::clamp_max(*pa_out, crange.y);
+      },
+      node.cfg().cm_cpu);
+
+  // --- Post-process
+
+  if (remap)
+    p_out->remap(0.f, 1.f, node.cfg().cm_cpu);
+
+  post_process_heightmap(node, *p_out, p_in);
 }
 
 } // namespace hesiod
