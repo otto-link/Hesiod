5f8b024c2f5c14dcae7be8bace26bb6410e44fb2
Hendrik A. <patchbyte@ash.local>
2026-09-09 18:58:32 +0200

    Fixed flatpak using wrong ICD

--- scripts/generate_flatpak/AltBuild.sh
+++ scripts/generate_flatpak/AltBuild.sh
@@ -1,1 +1,2 @@
+flatpak run --command=git org.flatpak.Builder config --global url."https://github.com/".insteadOf "git@github.com:"
 flatpak run org.flatpak.Builder --force-clean --user --repo repo/ build/ io.github.hesiod.yml
--- scripts/generate_flatpak/launch.sh
+++ scripts/generate_flatpak/launch.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-cd /app/hesiod
-./hesiod
+# Due to not being able to change the data dir, we have to get a bit creative
+export HESIOD_TMP_DIR=$(mktemp -d)
+cd $HESIOD_TMP_DIR
+
+ln -s /app/hesiod/data data
+ln -s /app/hesiod/hesiod hesiod
+
+export QTWEBENGINEPROCESS_PATH=/app/bin/QtWebEngineProcess
+
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libOpenCL.so.1 ./hesiod
